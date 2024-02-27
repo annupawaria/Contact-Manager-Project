3b3e83e25f784f7766ee87879d815c3023ee3aae
annupawaria <134483932+annupawaria@users.noreply.github.com>
2024-02-27 15:21:05 +0530

    new chages

--- src/components/ContactList.js
+++ src/components/ContactList.js
@@ -1,31 +1,22 @@
-// import { Delete } from '@mui/icons-material';
-import React from 'react';
-
-const ContactList = ({ contact }) => {
-  if (!contact || !Array.isArray(contact)) {
-    // If not, you can handle this situation, for example, by rendering a message
-    return <div>No contacts available</div>;
-  }
-  console.log(contact,'from contactList')
-  // Renamed the variable to avoid naming conflict
-  const contactList =  contact.map((val) => {
-    return (
-      <div key={val.id}>
-        <div>{val.name}</div>
-        <div>{val.email}</div>
-        {/* <span><DeleteIcon/></span> */}
-      </div>
-    );
-  });
-
+import React from 'react'
+import DeleteIcon from '@mui/icons-material/Delete';
+const ContactList = ({contact}) => {
+// console.log(contact,'r contactList')
+const contactList=contact.map((val)=>{
+  return(
+    <div className='contacts' >
+      <div>{val.name}</div>
+      <div>{val.email}</div>
+      {/* <DeleteIcon/> */}
+    </div>
+  )
+})
   return (
-    <>
     <div>
       <h1>ContactList</h1>
       <div>{contactList}</div>
     </div>
-    </>
-  );
-};
+  )
+}
 
-export default ContactList;
+export default ContactList
